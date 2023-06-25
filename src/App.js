47d7045edcda93b7ff257c4c4153193dea4c93f0
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2023-06-25 14:01:47 +0300

    working with Http requests

--- src/App.js
+++ src/App.js
@@ -1,33 +1,70 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import NewTasks from "./components/NewTasks/NewTasks";
 
 import Tasks from "./components/Tasks/Tasks";
 
-const All_Tasks = [
-  {
-    id: "1",
-    title: "проект по реакту.js",
-    range: 100,
-    date: new Date(2023, 7, 14),
-    istrue: true,
-  },
-  {
-    id: "2",
-    title: "Project 2 React",
-    range: 20,
-    date: new Date(2024, 6, 2),
-    istrue: true,
-  },
-];
-
 const App = () => {
-  const [tasks, setTasks] = useState(All_Tasks);
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const [isTaskYear, getIsTaskYear] = useState("2023");
-  const addTasksHandler = (task) => {
-    setTasks((prevStat) => {
-      return [task, ...prevStat];
-    });
+
+  /* Fetching tasks */
+  const fetchTasksHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        "https://react-http-a5d84-default-rtdb.firebaseio.com/taskslist.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const key in data) {
+        loadedTasks.push({
+          id: key,
+          title: data[key].title,
+          range: data[key].range,
+          date: new Date(data[key].date),
+          istrue: data[key].istrue,
+        });
+      }
+
+      setTasks(loadedTasks);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchTasksHandler();
+  }, [fetchTasksHandler]);
+
+  /* Adding tasks */
+  const addTasksHandler = async (task) => {
+    const response = await fetch(
+      "https://react-http-a5d84-default-rtdb.firebaseio.com/taskslist.json",
+      {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+
+    console.log(data);
   };
 
   const deleteElementwithId = (idtodelete) => {
@@ -49,16 +86,32 @@ const App = () => {
     getIsTaskYear(fullyear);
   };
 
-  return (
-    <>
-      <h1 className="App">📓Список задач : {isTaskYear}</h1>
-      <NewTasks onaddTasksHandler={addTasksHandler} />
+  let content = <p>Found no Tasks.</p>;
+
+  if (tasks.length > 0) {
+    content = (
       <Tasks
         ArrayTasks={tasks}
         ontasknumbersinyear={tasknumbersinyear}
         ondeleteElementwithId={deleteElementwithId}
         onsaveChangeElementWithId={saveChangeElementWithId}
       />
+    );
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h1 className="App"> Список задач : {isTaskYear}</h1>
+      <NewTasks onaddTasksHandler={addTasksHandler} />
+      {content}
     </>
   );
 };
